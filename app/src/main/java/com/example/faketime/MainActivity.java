9ae7d34839220de85a233b1ef2807662d62989cd
KilianM26 <43299260+KilianM26@users.noreply.github.com>
2020-02-26 14:26:50 -0500

    Update MainActivity.java

--- app/src/main/java/com/example/faketime/MainActivity.java
+++ app/src/main/java/com/example/faketime/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.faketime;
 
+import android.bluetooth.BluetoothAdapter;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -7,6 +9,8 @@ import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    public BluetoothAdapter bluetoothAdapter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,9 +21,20 @@ public class MainActivity extends AppCompatActivity {
         connectBluetooth.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
+                connectBluetooth();
             }
         });
+
+    }
+
+    public void connectBluetooth(){
+
+        bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        if (!bluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, 1);
+        }
 
     }
 }
