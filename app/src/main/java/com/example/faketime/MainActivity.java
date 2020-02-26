b1a64f6a7611399af93d4ba5462dade8e5dde185
KilianM26 <43299260+KilianM26@users.noreply.github.com>
2020-02-26 12:51:59 -0500

    Began trying to implement Bluetooth

--- app/src/main/java/com/example/faketime/MainActivity.java
+++ app/src/main/java/com/example/faketime/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.faketime;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button connectBluetooth = (Button) findViewById(R.id.connectBluetoothButton);
+        connectBluetooth.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
     }
 }
