0600ebf05dd163e04f9b9d315c50233701cc847a
KilianM26 <43299260+KilianM26@users.noreply.github.com>
2020-03-07 19:40:38 -0500

    idk man it doesn't really work
    i dont remember what i did. pushing after opening my laptop several days after i actually made the changes

--- app/src/main/java/com/example/faketime/CallPage.java
+++ app/src/main/java/com/example/faketime/CallPage.java
@@ -1,23 +1,30 @@
 package com.example.faketime;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
-import android.widget.VideoView;
 import android.view.ViewGroup.LayoutParams;
+import android.widget.VideoView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 //ths is the code that gets run when a call is made
 public class CallPage extends AppCompatActivity {
+
+    private final String TAG = "asdfghj.java";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.camera_preview);
 
+        Log.v(TAG, "Called the onCreate method");
         View view = (View) findViewById(R.id.VideoPreview);
         View parent = (View) view.getParent();
         int parent_width = parent.getWidth();
         int parent_height = parent.getHeight();
+
+        Log.v(TAG, "Created the view");
 
         //sizes the preview window to be the right size
         VideoView video = (VideoView) findViewById(R.id.VideoView);
@@ -25,6 +32,9 @@ public class CallPage extends AppCompatActivity {
         LayoutParams preview_layout = (LayoutParams) preview.getLayoutParams();
         preview_layout.width = parent_width/4;
         preview_layout.height = parent_height/4;
+
+        Log.v(TAG, "Changed preview dimensions");
+//        preview.setBackground(drawable.);
 
     }
 
