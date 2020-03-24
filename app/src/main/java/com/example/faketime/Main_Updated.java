43c1d0be0f148c991916a10410c84b69d6c63976
KilianM26 <43299260+KilianM26@users.noreply.github.com>
2020-03-24 12:54:54 -0400

    started preparing for the big change
    added some files, didn't change the actual behavior of the app yet.

---
+++ app/src/main/java/com/example/faketime/Main_Updated.java
@@ -0,0 +1,35 @@
+package com.example.faketime;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+
+import com.google.gson.Gson;
+
+import java.util.List;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Main_Updated extends AppCompatActivity {
+
+    SharedPreferences mPrefs;
+    SharedPreferences.Editor mEdit;
+
+    Gson gson = new Gson();
+    String json;
+    List contacts;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+
+        mPrefs = getSharedPreferences("state", 0);
+        mEdit = mPrefs.edit();
+        json = mPrefs.getString("MyObject", "");
+        contacts = gson.fromJson(json, List.class);
+
+        super.onCreate(savedInstanceState);
+
+
+
+    }
+
+}
