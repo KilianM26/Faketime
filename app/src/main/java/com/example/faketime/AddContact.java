f2ad31f4fcb3726e8f11d42feb569889bbb2f475
KilianM26 <43299260+KilianM26@users.noreply.github.com>
2020-03-24 22:41:12 -0400

    making stuff start to maybe work

--- app/src/main/java/com/example/faketime/AddContact.java
+++ app/src/main/java/com/example/faketime/AddContact.java
@@ -1,10 +1,7 @@
 package com.example.faketime;
 
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import com.google.gson.Gson;
 
@@ -12,7 +9,7 @@ import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Main_Updated extends AppCompatActivity {
+public class AddContact extends AppCompatActivity {
 
     SharedPreferences mPrefs;
     SharedPreferences.Editor mEdit;
@@ -31,14 +28,6 @@ public class Main_Updated extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
 
-        Button addContactButton = (Button) findViewById(R.id.addContactButton);
-        addContactButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent addContact = new Intent(Main_Updated.this, AddContact.class);
-                Main_Updated.this.startActivity(addContact);
-            }
-        });
 
 
     }
