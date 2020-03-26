582f401ea0422ae6b76ddb0d9912616ee6fe25fb
KilianM26 <43299260+KilianM26@users.noreply.github.com>
2020-03-26 12:43:17 -0400

    casting issues i'll take care of later

--- app/src/main/java/com/example/faketime/AddContact.java
+++ app/src/main/java/com/example/faketime/AddContact.java
@@ -3,12 +3,14 @@ package com.example.faketime;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -22,6 +24,8 @@ public class AddContact extends AppCompatActivity {
     String json;
     List<String[]> contacts;
     String[] curContact = new String[2];
+    String theme;
+    private static final String TAG = "asdfgh";
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -29,10 +33,21 @@ public class AddContact extends AppCompatActivity {
         mPrefs = getSharedPreferences("state", 0);
         mEdit = mPrefs.edit();
         json = mPrefs.getString("MyObject", "");
+        theme = mPrefs.getString("theme", "light");
         contacts = gson.fromJson(json, List.class);
+        if(contacts == null){
+            contacts = new ArrayList<String[]>();
+        }
+
+        //Sets the theme
+        if(theme.equals("dark")){
+            setTheme(R.style.Dark);
+        }else{
+            setTheme(R.style.Light);
+        }
 
         super.onCreate(savedInstanceState);
-
+        setContentView(R.layout.add_contact);
 
         final EditText contactName = (EditText) findViewById(R.id.contactName);
         final EditText contactAddress = (EditText) findViewById(R.id.contactAddress);
@@ -44,9 +59,16 @@ public class AddContact extends AppCompatActivity {
                 curContact[1] = contactAddress.getText().toString();
                 contacts.add(curContact);
 
+                Log.v(TAG, "Added name to contacts");
+                Log.v(TAG, "contacts[0] is " + curContact[0]);
+
                 json = gson.toJson(curContact);
+                Log.v(TAG, "json is " + json);
                 mEdit.putString("contacts", json);
                 mEdit.apply();
+
+                Intent mainActivity = new Intent(AddContact.this, Main_Updated.class);
+                AddContact.this.startActivity(mainActivity);
             }
         });
 
@@ -55,7 +77,7 @@ public class AddContact extends AppCompatActivity {
 
     @Override
     public void onBackPressed(){
-        Intent mainActivity = new Intent(AddContact.this, MainActivity.class);
+        Intent mainActivity = new Intent(AddContact.this, Main_Updated.class);
         AddContact.this.startActivity(mainActivity);
     }
 
--- app/src/main/java/com/example/faketime/Main_Updated.java
+++ app/src/main/java/com/example/faketime/Main_Updated.java
@@ -3,36 +3,53 @@ package com.example.faketime;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.gson.Gson;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.constraintlayout.widget.ConstraintLayout;
 
 public class Main_Updated extends AppCompatActivity {
 
     SharedPreferences mPrefs;
     SharedPreferences.Editor mEdit;
+    private static final String TAG = "asdfgh";
 
     Gson gson = new Gson();
     String json;
     List<String[]> contacts;
+    String theme;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
 
         mPrefs = getSharedPreferences("state", 0);
         mEdit = mPrefs.edit();
-        json = mPrefs.getString("MyObject", "");
-        contacts = gson.fromJson(json, List.class);
+        json = mPrefs.getString("contacts", "");
+        contacts = gson.fromJson(json, ArrayList.class);
+        theme = mPrefs.getString("theme", "light");
+
+
+        //Sets the theme
+        if(theme.equals("dark")){
+            setTheme(R.style.Dark);
+        }else{
+            setTheme(R.style.Light);
+        }
 
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.main_updated);
 
-        Button addContactButton = (Button) findViewById(R.id.addContactButton);
+        FloatingActionButton addContactButton = (FloatingActionButton) findViewById(R.id.addContactButton);
         addContactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -41,9 +58,23 @@ public class Main_Updated extends AppCompatActivity {
             }
         });
 
-        ConstraintLayout layout;
-        for(Object cur: contacts){
-            layout = new ConstraintLayout(this);
+        LinearLayout insideScroll = findViewById(R.id.scrollViewInternal);
+        LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+        TextView contactName = new TextView(this);
+        contactName.setLayoutParams(params);
+        Button callContact = new Button(this);
+        Button messageContact = new Button(this);
+        params.setMarginEnd(100);
+        if(!(contacts == null)) {
+            Log.v(TAG, "Contacts aren't null");
+            for (String[] cur : contacts) {
+                contactName.setText(cur[0]);
+                Log.v(TAG, "Contact Name is " + contactName);
+                insideScroll.addView(contactName);
+                Log.v(TAG, "Added TextView to LinearLayout");
+            }
+        }else{
+            Log.v(TAG, "Contacts ARE null");
         }
 
 
